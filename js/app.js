fe948261c93ac83507bd8bc39c46207865342f49
Leanne <leanne.thng@gmail.com>
2020-01-23 14:15:13 -0800

    Quick component setup overview

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+const Title = props => (
+  <h1 className="tc f1 blue pa3">
+    {props.title}
+  </h1>
+)
 
-ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
+const Banner = props =>( 
+  <h1 className="tc f1 yellow pa3">
+    Hello {props.firstName} {props.surname} 
+  </h1>
+);
+
+
+const App = () => (
+  <div>
+    <Title title="Welcome to this Site" />
+    <Banner firstName="Leanne" surname="T" />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
