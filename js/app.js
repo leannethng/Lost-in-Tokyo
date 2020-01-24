22f0c613a2a3d175675c8649c28e39d739c1be54
Leanne <leanne.thng@gmail.com>
2020-01-24 15:58:36 -0800

    Build navigation from data
    Using map to loop over data to create a list of elements for Nav.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
+const Nav = () =>(
+  <nav className="pt3 pt4-ns mb4 mb0-ns">
+    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
+      {menu.map(item => (
+        <li className={`mh2-ns f6 f4-1 tc ${item.className}`}>
+          <a className='white no-underline' href={item.href}>{item.children}</a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+
 const Highlight = props => (
   <span className={`relative highlight highlight-${props.color}`}>
     <span className="relative z-2">{props.children}</span>
   </span>
-)
+);
 
 const Intro = () => (
   <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
@@ -14,14 +27,12 @@ const Intro = () => (
       From <Highlight color="blue">museums</Highlight> and <Highlight color="blue">galleries</Highlight>, to <Highlight color="pink">robot restaurants</Highlight> and <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that keeps on giving. <Highlight color="yellow">Dattebayo!</Highlight>
     </div>
   </div>
-)
-
-
-
+);
 
 const App = () => (
   <div>
     <div className="min-vh-100 ph4 flex flex-column">
+      <Nav />
       <Intro />
       {/* Navigation component  */}
       {/* Intro text component */}
@@ -32,6 +43,6 @@ const App = () => (
 
     </div>
   </div>
-)
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
